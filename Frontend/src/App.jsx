dcc379b8e93d02faf55ed135d1083a1ff4b2f978
Sunil-Harde <158262002+Sunil-Harde@users.noreply.github.com>
2025-08-12 11:38:03 +0530

    Home Page Update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,9 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+
   const [count, setCount] = useState(0)
 
+
   return (
+    
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
